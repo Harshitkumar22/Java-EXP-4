d0219837b6c78b34793c8890f1efa035fcfd274c
Harshitkumar22 <harshitkumar1463@gmail.com>
2025-02-03 18:13:06 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,24 @@
+class Car{
+    private String make;
+    private String model;
+    private int year;
+    
+    public Car(String make, String model, int year){  // Parameterized constructor using 'this' to distinguish instance variables
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayCarDetails(){      // Method to display car details
+        System.out.println("Car Details:");
+        System.out.println("Make: " + this.make);
+        System.out.println("Model: " + this.model);
+        System.out.println("Year: " + this.year);
+    }
+
+    public static void main(String[] args){       // Creating an instance of Car with specific values
+        Car myCar=new Car("TATA NEXON", "Model 1", 2024);
+
+        myCar.displayCarDetails();      // Displaying car details
+    }
+}
