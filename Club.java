204c052fdef39f674966af8bc3a8a40a44bd99dd
Harshitkumar22 <harshitkumar1463@gmail.com>
2025-02-04 09:50:37 +0530

    Update Club.java

--- Club.java
+++ Club.java
@@ -1,3 +1,9 @@
+//3. Create a Club class with a static variable clubName and a non-static variable memberName.
+//Provide a static method displayClubName() to print the club’s name.Create multiple Club objects and assign different member names to each.
+//Objective: Show how the static variable clubName is shared across all instances, while memberName remains unique to each object. 
+//Prove this by displaying each member’s name alongside the shared club name.
+
+
 class Club{
     private static String clubName = "Tech Enthusiasts Club";  // Static variable
 
