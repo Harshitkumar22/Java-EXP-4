d090c9d1cfb8f7acebf8f95690a883f666448a6a
Harshitkumar22 <harshitkumar1463@gmail.com>
2025-02-04 09:27:53 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,3 +1,11 @@
+//1. Declare a Customer class with a private variable balance.
+//Implement a public addBalance(double amount) method to add funds.
+//Implement a protected deductBalance(double amount) method to reduce funds.
+//Define a default-access (package-private) method showBalance() to display the current balance.
+//Overload the addBalance(...) method to also accept an int amount for smaller deposits.
+//Objective: Create an instance of the Customer class Showcase method overloading for different deposit parameters.
+
+
 class Customer{
     private double balance;
     
