46244e9fde87bf018230f3311203a0b7d9dae94f
Harshitkumar22 <harshitkumar1463@gmail.com>
2025-02-04 09:49:42 +0530

    Add files via upload

---
+++ Club.java
@@ -0,0 +1,30 @@
+class Club{
+    private static String clubName = "Tech Enthusiasts Club";  // Static variable
+
+    private String memberName;
+
+    public Club(String memberName){   // Constructor to initialize memberName
+        this.memberName=memberName;
+    }
+
+    public static void displayClubName(){   // Static method to display club name
+        System.out.println("Club Name: " + clubName);
+    }
+
+    public void displayMemberDetails(){
+        System.out.println("Member Name: " + memberName + " | Club: " + clubName);
+    }
+
+    public static void main(String[] args){
+        Club.displayClubName();
+
+        // Creating multiple objects with different member names
+        Club member1 = new Club("Harshit");
+        Club member2 = new Club("Dhairya");
+        Club member3 = new Club("Prince ");
+
+        member1.displayMemberDetails();
+        member2.displayMemberDetails();
+        member3.displayMemberDetails();
+    }
+}
